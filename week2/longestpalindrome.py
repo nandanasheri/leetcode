42c33f29ed2f9c8b8d285e4ddda472f12c71185a
Nandana <nandanasheri@gmail.com>
2024-08-26 18:16:04 -0500

    longest palindrome hashmap

---
+++ week2/longestpalindrome.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        countmap = {}
+        for i in s:
+            if i not in countmap:
+                countmap[i] = 1
+            else:
+                countmap[i] += 1
+        
+        length = 0
+        flag = False
+        for i in countmap:
+            if countmap[i] % 2 == 0:
+                length += countmap[i]
+            else:
+                # add highest possible count of even letters
+                length += countmap[i] - 1
+                flag == true
+        
+        #there was at least one odd number of letters so we can place one letter in the middle of the palindrome
+        if flag:
+            return length+1
+        else:
+            return length
+
+        
