55798cd5dd7a850c67d73bd78bf68fa36b24edf7
Nandana <nandanasheri@gmail.com>
2025-06-05 12:10:27 -0500

    fumbled

---
+++ stack/739.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/daily-temperatures/description/
+# o(n)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        output = [0] * len(temperatures)
+        stack = []
+
+        for i in range(0, len(temperatures)):
+            temp = temperatures[i]
+            while len(stack) > 0 and stack[-1][0] < temp:
+                top = stack.pop()
+                ind = top[1]
+                output[ind] = i - ind
+            stack.append((temp, i))
+        return output
