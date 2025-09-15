2061ca87c25c6fa6ae2837e8a14a07565806d7d1
nandanasheri <nandanasheri@gmail.com>
2025-09-14 22:59:01 -0400

    lru revise

--- linkedlist/146.py
+++ linkedlist/146.py
@@ -1,62 +1,70 @@
 class Node:
-    def __init__(self, key, val):
+    def __init__ (self, key, value):
         self.key = key
-        self.val = val
+        self.value = value
         self.next = None
         self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.head = Node (0, 0)
-        self.tail = Node (0, 0)
-        # head - least recent, tail - most recent
+        self.capacity = capacity
+        self.head = Node(-1, -1)
+        self.tail = Node(-1, -1)
         self.head.next = self.tail
         self.tail.prev = self.head
         self.map = {}
-        self.capacity = capacity
     
-    def print_list(self):
+    def print(self):
         curr = self.head
         while curr:
-            print(curr.val, end = "  ")
+            print(curr.key, curr.value, end = ";")
             curr = curr.next
         print()
-
-    # remove any node
-    def remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-
-    # insert at right
-    def insert(self, node):
-        previous = self.tail.prev
-        previous.next = node
-        self.tail.prev = node
-        node.next = self.tail
-        node.prev = previous
+    
+    def remove(self, currNode):
+        # remove currNode from wherever it is
+        currNode.prev.next = currNode.next
+        currNode.next.prev = currNode.prev
+        self.map.pop(currNode.key)
+    
+    def insert(self, currNode):
+        self.tail.prev.next = currNode
+        currNode.prev = self.tail.prev
+        self.tail.prev = currNode
+        currNode.next = self.tail
+        self.map[currNode.key] = currNode
 
     def get(self, key: int) -> int:
-        # self.print_list()
-        if key not in self.map:
+        # self.print()
+        if key in self.map:
+            currNode = self.map[key]
+            # remove currNode from wherever it is
+            self.remove(currNode)
+            # move it to the end of cache right behind the tail
+            self.insert(currNode)
+            return self.map[key].value
+        else:
             return -1
-        self.remove(self.map[key])
-        self.insert(self.map[key])
-        return self.map[key].val
+        
 
     def put(self, key: int, value: int) -> None:
-        # self.print_list()
-       
-        # add to doubly linked list
+        # self.print()
         if key in self.map:
             self.remove(self.map[key])
-        self.map[key] = Node(key,value)
-        self.insert(self.map[key])
+        
+         # if at capacity, delete LRU
+        if len(self.map) == self.capacity:
+            # delete node from the front
+            delNode = self.head.next
+            self.remove(delNode)
+        
+        # add a node to the end of the cache - most recently used
+        newNode = Node(key, value)
+        self.insert(newNode)
+            
+        
 
-        if len(self.map) > self.capacity:
-            lru = self.head.next
-            self.remove(lru)
-            self.map.pop(lru.key)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
