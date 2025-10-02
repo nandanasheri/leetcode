5d75b1536e046745360e8a1fca6cfd5bea4a2e7c
nandanasheri <nandanasheri@gmail.com>
2025-10-01 23:31:47 -0400

    redid from mock

--- slidingwindow/3.py
+++ slidingwindow/3.py
@@ -1,22 +1,22 @@
-# figured out the algo was only missing a reset condition!!
-# https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_length = 0
-        if len(s) == 0:
-            return 0
-        start, end = 0,0
-
         chars = set()
 
-        while end < len(s):
-            if s[end] not in chars:
-                chars.add(s[end])
-                max_length = max(max_length, end-start + 1)
-                end += 1
-            else:
-                while s[end] in chars:
-                    chars.remove(s[start])
-                    start += 1
-    
-        return max_length
+        res = 0
+        if len(s) == 0:
+            return 0
+        if len(s) == 1:
+            return 1
+
+        l, r = 0, 1
+        chars.add(s[l])
+
+        while r < len(s):
+            while s[r] in chars:
+                chars.remove(s[l])
+                l += 1
+            res = max(res, r-l+1)
+            chars.add(s[r])
+            r += 1
+        
+        return res
