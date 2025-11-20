6ef1643b793d3feb312be4cadddb80b492eba157
nandanasheri <nandanasheri@gmail.com>
2025-11-20 12:31:51 -0500

    a small gotcha

---
+++ slidingwindow/713.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        prod = 1
+        l = 0
+        res = 0
+        for r in range(len(nums)):
+            prod *= nums[r]
+            while l <= r and prod >= k:
+                prod /= nums[l]
+                l += 1
+            res += (r-l+1)
+        return res
+            
