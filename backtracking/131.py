8ae6ce19291fd0f067661a32d523ab8317e8552b
nandanasheri <nandanasheri@gmail.com>
2025-11-23 15:35:09 -0500

    backtrack palindrome

--- backtracking/131.py
+++ backtracking/131.py
@@ -1,26 +1,18 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        result = []
-        part = []
+        res = []
+        curr = []
 
         def dfs(i):
-            if i >= len(s):
-                result.append(part.copy())
+            if i == len(s):
+                res.append(curr[::])
                 return
             for j in range(i, len(s)):
-                if isPalindrome(i,j):
-                    part.append(s[i:j+1])
+                curr_s = s[i:j+1]
+                # if valid palindrome partition
+                if curr_s == curr_s[::-1]:
+                    curr.append(curr_s)
                     dfs(j+1)
-                    part.pop()
-        
-        def isPalindrome(i,j):
-            # print(i,j)
-            while i < j:
-                if s[i] != s[j]:
-                    return False
-                i += 1
-                j -= 1
-            return True
-        
+                    curr.pop()
         dfs(0)
-        return result
+        return res
