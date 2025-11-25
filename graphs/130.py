53556f5731b79e780c0ed2f9ad38c94ea0b0f745
nandanasheri <nandanasheri@gmail.com>
2025-11-24 20:52:07 -0500

    surrounded regions

--- graphs/130.py
+++ graphs/130.py
@@ -3,36 +3,25 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-
         m = len(board)
         n = len(board[0])
-        visited = set()
-
         def dfs(i,j):
-            if i < 0 or j < 0 or i >= m or j >= n or board[i][j] != "O":
+            if i < 0 or i>=m or j < 0 or j>=n or board[i][j] != "O": 
                 return
-            board[i][j] = "T"
-            dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j+1)
-            dfs(i,j-1)
-        
-        # run dfs solely for border O's to get unsurrounded regions
-        for i in range(n):
-            dfs(0, i)
-            dfs(m-1, i)
+            board[i][j] = "A"
+            for x,y in [(1,0), (0,1), (-1,0), (0,-1)]:
+                dfs(i+x, j+y)
         
         for i in range(m):
-            dfs(i, 0)
-            dfs(i, n-1)
-
-        # print(board)
+            dfs(i,0)
+            dfs(i,n-1)
+        for i in range(n):
+            dfs(0,i)
+            dfs(m-1,i)
+        
         for i in range(m):
             for j in range(n):
                 if board[i][j] == "O":
-                    
                     board[i][j] = "X"
-                elif board[i][j] == "T":
+                elif board[i][j] == "A":
                     board[i][j] = "O"
-        
-        return
