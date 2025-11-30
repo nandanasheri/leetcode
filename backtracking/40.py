630c12835666d5d536728f3ff9d9067c1135644c
nandanasheri <nandanasheri@gmail.com>
2025-11-30 14:22:30 -0500

    combination sums

--- backtracking/40.py
+++ backtracking/40.py
@@ -1,26 +1,30 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
-        result = []
         curr = []
-
-        def dfs(i, currsum):
-
+        currsum = 0
+        result = []
+        candidates.sort()
+    
+        def dfs(i,currsum):
+            # print(i, curr)
             if currsum == target:
                 result.append(curr.copy())
                 return
-            if i == len(candidates) or currsum > target:
+            if currsum > target or i >= len(candidates):
                 return
-
-            # include candidates[i]
+            # we either choose to include the current number
             curr.append(candidates[i])
-            dfs(i+1, currsum + candidates[i])
+            # and we move on to the next - which could possible use the same value
+            dfs(i+1,currsum+candidates[i])
             curr.pop()
-
-            # skip candidates[i]
+            # or we choose to skip it
+            # we can't do what we do in CS1 because we just don't start a NEW call with same number
             while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
                 i += 1
+
             dfs(i+1, currsum)
 
+            
+        
         dfs(0,0)
         return result
