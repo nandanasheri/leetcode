81e4af8a2e7c31834808208145653311f8247321
nandanasheri <nandanasheri@gmail.com>
2025-12-11 10:59:40 -0500

    damn

--- slidingwindow/567.py
+++ slidingwindow/567.py
@@ -1,31 +1,30 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1):
+        map1 = {}
+        map2 = {}
+        for i in range(len(s2)):
+            map1[s2[i]] = 0
+            map2[s2[i]] = 0
+            
+        if len(s1) > len(s2):
             return False
-        freq1 = [0] * 26
-        freq2 = [0] * 26
 
         for i in range(len(s1)):
-            freq1[ord(s1[i]) - ord('a')] += 1
-            freq2[ord(s2[i]) - ord('a')] += 1            
+            map1[s1[i]] = map1.get(s1[i], 0) + 1
+            map2[s2[i]] += 1
 
-        l = 0
-        
-        for r in range(len(s1), len(s2)):
-            if freq1 == freq2:
+        l, r = 0, len(s1) - 1
+
+        while r < len(s2):
+            # print(map1, map2, l, r)
+            if map1 == map2:
                 return True
-            
-            index = ord(s2[r]) - ord('a')
-            freq2[index] += 1
-
-            index = ord(s2[l]) - ord('a')
-            freq2[index] -= 1
-
+            r += 1
+            if r == len(s2):
+                break
+            map2[s2[r]] += 1
+            map2[s2[l]] -= 1
             l += 1
-        
-        return freq1 == freq2
+        return False
 
-        # also you can do this using a matches variable so it goes from o(26n) to o(n+26)
-
-
-        
+            
