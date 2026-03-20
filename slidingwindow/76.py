d1bb5d04141b52eb8bd6e37de30a87df9e9f1abf
nandanasheri <nandanasheri@gmail.com>
2026-03-19 21:43:50 -0400

    i still got it

--- slidingwindow/76.py
+++ slidingwindow/76.py
@@ -1,39 +1,43 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        s_map = {}
-        t_map = {}
-        matches = 0
-        result = float("inf")
-        res_str = ""
+        t_freqMap = {}
+        s_freqMap = {}
+        
         for i in t:
-            t_map[i] = t_map.get(i,0) + 1
+            t_freqMap[i] = t_freqMap.get(i,0) + 1
         
+        minlen = float("inf")
+        result = ""
         l = 0
-        for r in range(len(s)):
-            # print(l,r, s_map)
-            char = s[r]
-            # if in t -> add to map and check for a match
-            if char in t_map:
-                s_map[char] = s_map.get(char, 0) + 1 
-                if s_map[char] == t_map[char]:
-                    matches += 1
-            # print(matches)
-            # shift window until it's invalid again if valid window
-            while matches == len(t_map):
-                if result > r-l+1:
-                    result = r-l+1
-                    res_str = s[l:r+1]
-                char = s[l]
-                if char in s_map:
-                    s_map[char] -= 1
-                    if s_map[char] < t_map[char]:
-                        matches -= 1
-                l += 1
-            
-            # shift so boundary is always a match for minimum
-            while l < len(s) and s[l] not in t_map:
-                l += 1
-        
-        return res_str
-            
+        r = 0
 
+        # o(1) because maps are fixed size
+        def is_match():
+            for i in t_freqMap:
+                if i not in s_freqMap:
+                    return False
+                if s_freqMap[i] < t_freqMap[i]:
+                    return False
+            return True
+
+        # slide through your window
+        while l < len(s) and r < len(s):
+            # we always want our window to start at an important char for minimum
+            if s[l] not in t_freqMap:
+                l += 1
+                continue
+            # add to freqMap if important
+            if s[r] in t_freqMap:
+                s_freqMap[s[r]] = s_freqMap.get(s[r], 0) + 1
+            
+            # check for a match and keep updating result if you find a match
+            while is_match():
+                if r-l+1 < minlen:
+                    minlen = r-l+1
+                    result = s[l:r+1]
+                if s[l] in s_freqMap:
+                    s_freqMap[s[l]] -= 1
+                l += 1
+            r += 1
+
+        return result
