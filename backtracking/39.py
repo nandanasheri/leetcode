f119ac62c3b0e90ad1d429992864dcdc2a229d5d
nandanasheri <nandanasheri@gmail.com>
2026-03-24 20:47:07 -0400

    combinations

--- backtracking/39.py
+++ backtracking/39.py
@@ -1,19 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
-        currsum = 0
         curr = []
 
-        def dfs(i,currsum):
-            # print(curr)
-            if currsum == target:
-                result.append(curr.copy())
+        def _combinationSum(i, currsum):
+            if currsum > target or i == len(candidates):
                 return
-            if currsum > target:
+            if currsum == target:
+                result.append(curr[::])
                 return
             for j in range(i, len(candidates)):
                 curr.append(candidates[j])
-                dfs(j, currsum+candidates[j])
+                _combinationSum(j, currsum+candidates[j])
                 curr.pop()
-        dfs(0,0)
+        
+        _combinationSum(0,0)
         return result
+
+            
