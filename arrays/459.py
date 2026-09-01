bdf255388d4908b9378ed08c31735251c09a2a48
nandanasheri <nandanasheri@gmail.com>
2026-09-01 09:02:26 -0400

    back at it

--- arrays/459.py
+++ arrays/459.py
@@ -1,16 +1,12 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-
-        for i in range(0, len(s)//2):
+        for i in range(len(s)):
             substr = s[0:i+1]
-            sub_len = len(substr)
-            
-            matches = 0
-            for j in range(0, len(s), sub_len):
-                if s[j:j+sub_len] != substr:
-                    break
-                else:
-                    matches += 1
-            if matches == len(s) / sub_len:
+            if len(s) % len(substr) != 0:
+                continue
+            num = len(s) // len(substr)
+            if num == 1:
+                continue
+            if substr * num == s:
                 return True
         return False
