839164bc246dae5f72a819e7990f47a96909d587
nandanasheri <nandanasheri@gmail.com>
2026-09-23 20:23:36 -0400

    robeer

--- dp-1/213.py
+++ dp-1/213.py
@@ -1,35 +1,21 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-
-        def house_robber_i(nums):
-           
-            dp_cache = [0] * len(nums)
-            if len(nums) == 1:
-                return nums[0]
-            # adjacent - cannot rob either
-            if len(nums) == 2:
-                return max(nums[0], nums[1])
-            
-            dp_cache[-1] = nums[-1]
-            dp_cache[-2] = nums[-2]
-
-            curr_max = dp_cache[-1]
-            
-            for i in range(len(nums) - 3, -1, -1):
-                dp_cache[i] = nums[i] + curr_max
-                curr_max = max(curr_max, dp_cache[i+1])
-
-           
-            return max(dp_cache[0], dp_cache[1])
-
-        if len(nums) == 1:
-                return nums[0]
-
-        if len(nums) == 2:
-            return max(nums[0], nums[1])
-
-        # run house robber I twice on two different sub arrays excluding first and last house!
-        max_a = house_robber_i(nums[1:])
-        max_b = house_robber_i(nums[0:-1])
-
-        return max(max_a, max_b)
+    def rob(self, nums: list[int]) -> int:
+   
+        if len(nums) <= 3:
+            return max(nums)
+        
+        def _bottomUpRob(subarr):
+            dp_cache = [0] * len(subarr)
+            n = len(subarr)
+            # bottom up DP for first subarray
+            for i in range(n-1, -1, -1):
+                if i == n-1:
+                    dp_cache[i] = subarr[n-1]
+                    continue
+                if i == n-2:
+                    dp_cache[i] = max(subarr[i], dp_cache[i+1])
+                    continue
+                dp_cache[i] = max(subarr[i] + dp_cache[i+2], dp_cache[i+1])
+            return dp_cache[0]
+        
+        return max(_bottomUpRob(nums[:-1]), _bottomUpRob(nums[1:]))
